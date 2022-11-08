ef5d549bc71168209b0c3d3f7a08edad97470ba1
Mikołaj <frankiewiczmikolaj@gmail.com>
2022-11-08 22:43:18 +0100

    The if-else conditional statement, logical AND and OR operators

--- src/dev/frankiewicz/Main.java
+++ src/dev/frankiewicz/Main.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name:");
         String name = scanner.nextLine();
-        System.out.println("Hello "+name+"!");
+
+        if (!name.isEmpty() && (!name.endsWith("a") || name.equals("Barnaba") || name.equals("Kuba"))){
+            System.out.println("Hello "+name+"!");
+            System.out.println("You are man.");
+        }else if(!name.isEmpty() && name.endsWith("a")){
+            System.out.println("Hello "+name+"!");
+            System.out.println("You are woman.");
+        }else {
+            System.out.println("You must enter your first name. Name cannot be empty!");
+        }
     }
 }

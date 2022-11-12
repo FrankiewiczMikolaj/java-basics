9d5da6ddeb08459233588cef6b980e51b82e67a8
Mikołaj <frankiewiczmikolaj@gmail.com>
2022-11-12 20:14:10 +0100

    Concatenation of strings using the "+" operator

--- src/dev/frankiewicz/Main.java
+++ src/dev/frankiewicz/Main.java
@@ -5,17 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name:");
-        String name = scanner.nextLine();
-
-        if (!name.isEmpty() && (!name.endsWith("a") || name.equals("Barnaba") || name.equals("Kuba"))){
-            System.out.println("Hello "+name+"!");
-            System.out.println("You are man.");
-        }else if(!name.isEmpty() && name.endsWith("a")){
-            System.out.println("Hello "+name+"!");
-            System.out.println("You are woman.");
-        }else {
-            System.out.println("You must enter your first name. Name cannot be empty!");
-        }
+//        System.out.println("Enter your name:");
+//        String name = scanner.nextLine();
+//
+//        if (!name.isEmpty() && (!name.endsWith("a") || name.equals("Barnaba") || name.equals("Kuba"))){
+//            System.out.println("Hello "+name+"!");
+//            System.out.println("You are man.");
+//        }else if(!name.isEmpty() && name.endsWith("a")){
+//            System.out.println("Hello "+name+"!");
+//            System.out.println("You are woman.");
+//        }else {
+//            System.out.println("You must enter your first name. Name cannot be empty!");
+//        }
+//        System.out.println("Enter your age:");
+//        int age = scanner.nextInt();
+//        if(age >= 18){
+//            System.out.println("You are an adult.");
+//        }else{
+//            System.out.println("You are underage.");
+//        }
+        //String Concatenation in Java
+        String firstName = "Mikołaj";
+        String lastName = "Frankiewicz";
+        System.out.println(firstName+" "+lastName);
     }
 }

bbfb7a30016d4fd365df61a8c2823b20630bc29a
Mikołaj <frankiewiczmikolaj@gmail.com>
2022-11-24 20:36:39 +0100

    Comparison operators, boolean variables

--- src/dev/frankiewicz/Main.java
+++ src/dev/frankiewicz/Main.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Definition of the scanner that will be used to capture data entered by the console
         Scanner scanner = new Scanner(System.in);
+        //region The if-else conditional statement, logical AND/OR operators
 //        System.out.println("Enter your name:");
 //        String name = scanner.nextLine();
-//
 //        if (!name.isEmpty() && (!name.endsWith("a") || name.equals("Barnaba") || name.equals("Kuba"))){
 //            System.out.println("Hello "+name+"!");
 //            System.out.println("You are man.");
@@ -17,6 +18,8 @@ public class Main {
 //        }else {
 //            System.out.println("You must enter your first name. Name cannot be empty!");
 //        }
+        //endregion
+        //region Checking if a person is an adult based on the entered age
 //        System.out.println("Enter your age:");
 //        int age = scanner.nextInt();
 //        if(age >= 18){
@@ -24,29 +27,47 @@ public class Main {
 //        }else{
 //            System.out.println("You are underage.");
 //        }
-//        //String Concatenation in Java
+        //endregion
+        //region String Concatenation
 //        String firstName = "Mikołaj";
 //        String lastName = "Frankiewicz";
 //        System.out.println(firstName+" "+lastName);
-        //Mathematical operators and numerical variables
-        int a = 2;
-        int b = 3;
-        System.out.println("a="+a+" b="+b);
-        System.out.println("Addition:"+(a+b));
-        System.out.println("Subtraction:"+(a-b));
-        System.out.println("Multiplication:"+(a*b));
-        System.out.println("Division of an int variable:"+(a/b));
-        double x = 2;
-        double y = 3;
-        System.out.println("\nx="+x+" y="+y);
-        System.out.println("Division of a double variable:"+(x/y));
-        a = 10;
-        b = 2;
-        System.out.println("\na="+a+" b="+b);
-        System.out.println("The rest of the division:"+(a%b));
-        a = 10;
-        b = 3;
-        System.out.println("\na="+a+" b="+b);
-        System.out.println("The rest of the division:"+(a%b));
+        //endregion
+        //region Mathematical operators and numerical variables
+//        int a = 2;
+//        int b = 3;
+//        System.out.println("a="+a+" b="+b);
+//        System.out.println("Addition:"+(a+b));
+//        System.out.println("Subtraction:"+(a-b));
+//        System.out.println("Multiplication:"+(a*b));
+//        System.out.println("Division:"+(a/b));
+//        double x = 2;
+//        double y = 3;
+//        System.out.println("\nx="+x+" y="+y);
+//        System.out.println("Division:"+(x/y));
+//        a = 10;
+//        b = 2;
+//        System.out.println("\na="+a+" b="+b);
+//        System.out.println("The rest of the division:"+(a%b));
+//        a = 10;
+//        b = 3;
+//        System.out.println("\na="+a+" b="+b);
+//        System.out.println("The rest of the division:"+(a%b));
+        //endregion
+        //region Comparison operators, boolean variables
+//        int numberA = 10;
+//        int numberB = 3;
+//        System.out.println(numberA > numberB);
+//        System.out.println(numberA < numberB);
+//        System.out.println(numberA == numberB);
+//        System.out.println(numberA != numberB);
+//
+//        boolean isSkyBlue = true;
+//        if (isSkyBlue){
+//            System.out.println("Sky is Blue");
+//        }else {
+//            System.out.println("Sky is not Blue");
+//        }
+        //endregion
     }
 }
